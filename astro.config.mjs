d469e9c0e69c9a196bb05e269764a9d6942ab752
Jonathan Gelin <gelin.jonathan@gmail.com>
2024-11-17 16:34:59 +0100

    feat: integrate styles in tailwind directly

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://smartsdlc.dev',
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  })],
+  integrations: [tailwind()],
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -14,9 +16,73 @@ export default {
     },
     extend: {
       boxShadow: {
-        'around': 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px',
-      }
-    }
+        around: 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px',
+      },
+      textColor: {
+        primary: {
+          DEFAULT: colors.green[700],
+          dark: colors.green[500],
+        },
+        secondary: {
+          DEFAULT: colors.slate[500],
+          dark: colors.slate[400],
+        },
+      },
+      // Border colors
+      borderColor: {
+        primary: {
+          DEFAULT: colors.green[700],
+          dark: colors.green[500],
+        },
+      },
+      // Text decoration colors
+      textDecorationColor: {
+        primary: {
+          DEFAULT: colors.green[700],
+          dark: colors.green[500],
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    // Plugin for the curved underline
+    function ({ addComponents }) {
+      addComponents({
+        '.curved-underline': {
+          position: 'relative',
+          '&:after': {
+            content: '""',
+            position: 'absolute',
+            left: '0',
+            width: '100%',
+            bottom: '-20px',
+            borderTopWidth: '4px',
+            borderRadius: '80%',
+            height: '20px',
+            borderColor: 'rgb(21 128 61)',
+            '.dark &': {
+              borderColor: 'rgb(34 197 94)',
+            },
+          },
+        },
+        '.curved-underline-slim': {
+          position: 'relative',
+          '&:after': {
+            content: '""',
+            position: 'absolute',
+            left: '0',
+            width: '100%',
+            bottom: '-20px',
+            borderTopWidth: '2px',
+            borderRadius: '80%',
+            height: '20px',
+            borderColor: 'rgb(21 128 61)',
+            '.dark &': {
+              borderColor: 'rgb(34 197 94)',
+            },
+          },
+        },
+      });
+    },
+  ],
 };
